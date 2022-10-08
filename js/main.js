e29293bff4027844e5cca12de4fcef350c719227
John Britton <johnbritton@Enterprise-3.local>
2022-10-07 22:11:42 -0600

    CSS styling added

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+// /*----- constants -----*/
+// Code Options {}
+// clue colors {}
+
+// /*----- state variables -----*/
+// board
+// option select
+
+// /*----- cached elements  -----*/
+// h1 - turns left
+// option buttons
+// enter button
+// replay button
+
+// /*----- event listeners -----*/
+// handle option click
+// handle enter click
+// handle replay click
+
+// /*----- functions -----*/
+// init
+// generate secret code order
+// handle clicks (3)
+// - show selected options
+// - Keep correct options in place.
+// - show correct/almost/no colors
+// - clear wrong options
+// - start next turn - based on winner, re-init
+// - win logic
+// - re-init on replay
+
+
+// Ice-box:
+// Play music
+// Play video on win/lose
+// opening message of rules/theme situation
