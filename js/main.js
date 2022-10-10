5d64cb63b7b45c7fc5861e34a668957b57bffb88
John Britton <johnbritton@Enterprise-3.local>
2022-10-10 17:41:23 -0600

    added secret code generator

--- js/main.js
+++ js/main.js
@@ -1,43 +1,100 @@
 
 // /*----- constants -----*/
-codeOptions  = {
-    '1': 'white rabbit Obj/',
-    '2': '%git.push**',
-    '3': 'pod2 security=>/',
-    '4': 'SEI/9/26${}',
-    '5': 'access/security/17-'
-}
+codeOptions  = [document.getElementById('option-1').innerText, document.getElementById('option-2').innerText,
+   document.getElementById('option-3').innerText, document.getElementById('option-4').innerText, 
+   document.getElementById('option-5').innerText, document.getElementById('option-6').innerText];
 
 /*----- state variables -----*/
 let board;
 let turn;
 let secretCode;
-
+let button;
 // /*----- cached elements  -----*/
-turnPow = document.querySelector('h1');
-options = document.getElementsByClassName('.answers');
-enterBtn = document.getElementById('#rtn');
-replayBtn = document.getElementById('#plyAgn');
+const turnPow = document.querySelector('h1');
+const options = document.querySelectorAll('.option');
+const enterBtn = document.getElementById('rtn');
+const replayBtn = document.getElementById('plyAgn');
+const boardEls = document.querySelectorAll('divs');
 
-// /*----- event listeners -----*/
-// handle option click
-// handle enter click
-// handle replay click
+// // /*----- event listeners -----*/
+document.getElementById('Choices').addEventListener('click', handleChoice);
+// document.getElementById('rtn').addEventListener('click', handleCheck);
+document.getElementById('plyAgn').addEventListener('click', init);
 
 // /*----- functions -----*/
-// init
-// generate secret code order
-// handle clicks (3)
-// - show selected options
-// - Keep correct options in place.
-// - show correct/almost/no colors
-// - clear wrong options
-// - start next turn - based on winner, re-init
-// - win logic
-// - re-init on replay
+init ();
+
+function init() {
+    board = [document.getElementById('answer-1').innerText = null, document.getElementById('answer-2'), document.getElementById('answer-3'), document.getElementById('answer-4')];
+    turn = 1;
+    winner = null;
+    getSecretCode()
+    render();
+
+};
+function render() {
+    // renderBoard();
+    // renderMessage();
+    renderReplay();
+}
+
+// // generate secret code order
+function getSecretCode() {
+    secretCode = [];
+  for (let i=0; i<4; i++){
+    secretCode.push(codeOptions[Math.floor(Math.random() * 5)]);
+}
+console.log(secretCode); 
+}
+
+
+// // handle clicks (3)
+function handleChoice(evt) {
+   const button = evt.target.innerText;
+   document.getElementById('answer-1').innerText = button;
+   if (document.getElementById('answer-1').innerText == button)
+   return;
+}
+//    document.getElementById('answer-1').innerText = button;
+// console.log(button)
+//     if ( ||
+//         winner)
+//     return;
+
+// function renderBoard() {
+//     board.forEach(function(idx) {
+//       const cellEl = document.getElementById(`answer-${idx}`);
+//       cellEl.css.innerHTML = button;
+//     });
+
+// function getWinner() {
+//     if ()
+// }
+    // - show selected options
+
+// } 
+// function handleCheck() {
+//     // - show correct/almost/no colors
+//     // - Keep correct options in place.
+//     // - clear wrong options
+//     // - start next turn - based on winner, re-init
+//     // - win logic
+
+// function renderBoard() {
+//     board.forEach(function(sqVal, idx) {
+//         const answerEl = document.getElementById('Choices');
+//         answerEl.innerText = 
+//     });
+// }
+function renderReplay() {
+    replayBtn.style.visibility = winner ? 'visible' : 'hidden';
+}
+//     // - re-init on replay
+    
+
 
 
 // Ice-box:
 // Play music
 // Play video on win/lose
-// opening message of rules/theme situation
+// opening message of rules/theme situatio
