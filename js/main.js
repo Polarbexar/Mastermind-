175d3d883826769536542a900159dc213d1f5ca2
John Britton <johnbritton@Enterprise-3.local>
2022-10-11 12:01:07 -0600

    Reworking secretcode Object, evt target ID

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
+// Previous Code Array
+// codeOptions  = [document.getElementById('option-1').innerText, document.getElementById('option-2').innerText,
+//    document.getElementById('option-3').innerText, document.getElementById('option-4').innerText, 
+//    document.getElementById('option-5').innerText, document.getElementById('option-6').innerText];
 
 // /*----- constants -----*/
-codeOptions  = [document.getElementById('option-1').innerText, document.getElementById('option-2').innerText,
-   document.getElementById('option-3').innerText, document.getElementById('option-4').innerText, 
-   document.getElementById('option-5').innerText, document.getElementById('option-6').innerText];
+codeOptions = {
+    '1' : 'white',
+    '2' : 'blue',
+    '3' : 'red',
+    '4' : 'gray',
+    '5' : 'yellow',
+    '6' : 'black',
+};
+// let result;
+let secretCode;
 
+function getSecretCode() {
+    secretCode = [];
+    for (let i=0; i<4; i++){
+        secretCode.push(codeOptions[Math.floor(Math.random() * 7)]);
+    }
+    console.log(secretCode); 
+}
+// getSecretCode(codeOptions, 4);
+
+// function getSecretCode(sourceArray, neededElements) {
+//     let secretCode = [];
+//     for (let i = 0; i < neededElements; i++) {
+//         secretCode.push(sourceArray[Math.floor(Math.random()*sourceArray.length)]);
+//     }
+//     return secretCode;
+    
+// }
+
+
+// const getRnd = (a, n) => new Array(n).fill(null).map(() => a[Math.floor(Math.random() * a.length)]);
+
+
+// const random_code = Math.floor(Math.random() * 4);
+// let randomObject = codeOptions2[Math.floor(Math.random() * 4)];
 /*----- state variables -----*/
 let board;
 let turn;
-let secretCode;
-let choices;
 let button;
 let winner; 
+
 // /*----- cached elements  -----*/
 const turnPow = document.querySelector('h1');
 const enterBtn = document.getElementById('rtn');
@@ -30,6 +64,7 @@ document.getElementById('plyAgn').addEventListener('click', init);
 init ();
 
 function init() {
+    board = [null, null, null, null]
     turn = 1;
     winner = false;
     getSecretCode()
@@ -37,7 +72,8 @@ function init() {
 };
 // handle clicks (3)
 function handleChoice(evt) {
-    button = evt.target.innerText;
+    button = evt.target.id.replace('option-','');
+    console.log(button);
     render()
 }
 function handleCheck() {
@@ -52,109 +88,118 @@ function render() {
     renderReplay();
 }
 function renderBoard() {
-    if ( answer1El.innerText == '') {
     answer1El.innerText = button;
-     answer1El.innerText = button;
-    }
-    choices.push[button]
+    
 }
 function getWinner() {
     if (answer1El.innerText === secretCode[0] && answer2El.innerText === secretCode[1] 
         && answer3El === secretCode[2] && answer4El === secretCode[3]) {
-        return winner = true;
-}
-}
-function colorCheck() {
-    if (answer1El.innerText == secretCode[0]) {
-        answer1El.style.color = 'green';
+            return winner = true;
+        }
     }
-     if (answer1El.innerText == secretCode[1] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'yellow';
+    function colorCheck() {
+        if (answer1El.innerText == secretCode[0]) {
+            answer1El.style.color = 'green';
+        }
+        if (answer1El.innerText == secretCode[1] || secretCode[2] || secretCode[3]) {
+            answer1El.style.color = 'yellow';
+        }
+        if (answer2El.innerText == secretCode[1]) {
+            answer1El.style.color = 'green';
+        }
+        if (answer2El.innerText == secretCode[0] || secretCode[2] || secretCode[3]) {
+            answer1El.style.color = 'yellow';
+        }
+        if (answer3El.innerText == secretCode[2]) {
+            answer1El.style.color = 'green';
+        }
+        if (answer3El.innerText == secretCode[0] || secretCode[1] || secretCode[3]) {
+            answer1El.style.color = 'yellow';
+        }
+        if (answer4El.innerText == secretCode[3]) {
+            answer1El.style.color = 'green';
+        }
+        if (answer4El.innerText == secretCode[0] || secretCode[1] || secretCode[2]) {
+            answer1El.style.color = 'yellow';
+        }
+        if (answer1El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
+            answer1El.style.color = 'black'; 
+        }
+        if (answer2El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
+            answer1El.style.color = 'black'; 
+        }
+        if (answer3El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
+            answer1El.style.color = 'black'; 
+        }
+        if (answer4El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
+            answer1El.style.color = 'black'; 
+        }
     }
-     if (answer2El.innerText == secretCode[1]) {
-        answer1El.style.color = 'green';
+    function renderMessage() { 
+        if (winner === true) {
+            turnPow.innerText = "You Saved The Park";
+        } else if (turn == 1) {
+            turnPow.innerText = '99% Power';
+        } else if (turn == 2) {
+            turnPow.innerText = "75% Power";
+        } else if (turn == 3) {
+            turnPow.innerText = "50% Power";
+        } else if (turn == 4) {
+            turnPow.innerText = "25% Power";
+            turnPow.style.color = 'yellow';
+        } else if (turn == 5) { 
+            turnPow.innerText = "Shutting Down";
+            turnPow.style.color = 'red';
+        }
+        
     }
-     if (answer2El.innerText == secretCode[0] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'yellow';
-    }
-     if (answer3El.innerText == secretCode[2]) {
-        answer1El.style.color = 'green';
-    }
-     if (answer3El.innerText == secretCode[0] || secretCode[1] || secretCode[3]) {
-        answer1El.style.color = 'yellow';
-    }
-     if (answer4El.innerText == secretCode[3]) {
-        answer1El.style.color = 'green';
-    }
-     if (answer4El.innerText == secretCode[0] || secretCode[1] || secretCode[2]) {
-        answer1El.style.color = 'yellow';
-    }
-    if (answer1El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'black'; 
-    }
-    if (answer2El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'black'; 
-    }
-    if (answer3El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'black'; 
-    }
-    if (answer4El.innerText !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'black'; 
-  }
-}
-function renderMessage() { 
-    if (winner === true) {
-    turnPow.innerText = "You Saved The Park";
-    } else if (turn == 1) {
-    turnPow.innerText = '99% Power';
-    } else if (turn == 2) {
-    turnPow.innerText = "75% Power";
-    } else if (turn == 3) {
-    turnPow.innerText = "50% Power";
-    } else if (turn == 4) {
-    turnPow.innerText = "25% Power";
-    turnPow.style.color = 'yellow';
-    } else if (turn == 5) { 
-    turnPow.innerText = "Shutting Down";
-    turnPow.style.color = 'red';
-    }
-
-}
-
-// - show selected options
-
-// // generate secret code order
-function getSecretCode() {
-    secretCode = [];
-    for (let i=0; i<4; i++){
-        secretCode.push(codeOptions[Math.floor(Math.random() * 5)]);
-    }
-    console.log(secretCode); 
-}
-
-// } 
-// function handleCheck() {
-//     // - clear wrong options
-//     // - win logic
-
+    
 // function renderBoard() {
 //     board.forEach(function(sqVal, idx) {
-//         const answerEl = document.getElementById('Choices');
-//         answerEl.innerText = 
-//     });
-// }
+    //         const answerEl = document.getElementById('Choices');
+    //         answerEl.innerText = 
+    //     });
+    // }
 function renderReplay() {
     replayBtn.style.visibility = winner ? 'visible' : 'hidden';
     replayBtn.style.visibility = turn == 5 ? 'visible' : 'hidden';
     allAnswers.style.visibility = turn == 5 || winner ? 'hidden' : 'visible';
     enterBtn.style.visibility = turn == 5 || winner ? 'hidden' : 'visible';
 }
-//     // - re-init on replay
+    //     // - re-init on replay
     
+    
+    
+    
+    // Ice-box:
+    // Play music
+    // Play video on win/lose
+    // opening message of rules/theme situatio
+    
+                
+                
+                
+                
+                
+                
+                
+                
+                // // generate secret code order - Previous getSecretCode
+                
+                
+                
 
 
 
-// Ice-box:
-// Play music
-// Play video on win/lose
-// opening message of rules/theme situatio
+
+
+
+
+
+
+
+
+
+
+
+
