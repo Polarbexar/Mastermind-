e5caf458155524ad48ef1afa555f2b21690809f1
John Britton <johnbritton@Enterprise-3.local>
2022-10-12 13:29:51 -0600

    Refactor majority of code

--- js/main.js
+++ js/main.js
@@ -7,14 +7,15 @@ codeOptions = {
    '4' : 'SEI/9/26${}',
    '5' : 'access/security/17-',
 };
-codeAnswers = ['', '', '' , '']
 
 /*----- state variables -----*/
 let turn;
 let winner; 
 let secretCode;
 let choice;
-let answer;
+let currentDiv;
+let playerGuess;
+let feedbackArray;
 
 // /*----- cached elements  -----*/
 const turnPow = document.querySelector('h1');
@@ -30,7 +31,7 @@ const allAnswers = document.getElementById('Choices');
 document.getElementById('Choices').addEventListener('click', handleChoice);
 document.getElementById('rtn').addEventListener('click', handleCheck);
 document.getElementById('plyAgn').addEventListener('click', init);
-document.getElementById('answerGrid').addEventListener('click', handleAnswer)
+document.getElementById('answerGrid').addEventListener('click', updateDiv)
 
 // /*----- functions -----*/
 init ();
@@ -38,109 +39,94 @@ init ();
 function init() {
     turn = 1;
     winner = false;
-    getSecretCode()
+    playerGuess = ['', '', '', ''];
+    currentDiv = 0;
+    choice = 0;
+    getSecretCode();
     render();
 };
-function handleAnswer(evt) {
-    answer = evt.target
+function updateDiv(evt) {
+    currentDiv = evt.target.id[7];
+    console.log(evt.target.id[7]);
 }
 
 function handleChoice(evt) {
-    choice = evt.target.innerText;
-    answer.innerText = choice;
+    playerGuess[currentDiv] = evt.target.id[7];
+    console.log(evt.target.id[7], playerGuess);
     render()
-    }
+}
 
 function handleCheck() {
-    turn++;
-    buildArray()
-    colorCheck1();
-    colorCheck2();
-    colorCheck3();
-    colorCheck4();
-    getWinner();
+    if (playerGuess.includes('')) return;
+    winner = getWinner();
+    if (!winner) setNextTurn();
     render();
 }
+
+function setNextTurn() {
+    turn++;
+    feedbackArray.forEach(function(val, idx) {
+        let div = document.getElementById(`answer-${idx}`)
+        div.style.color = val;
+    }) 
+}
+
 function render() {
+    updateDivs();
     renderMessage();
     renderReplay();
 }
-function buildArray() {
-    codeAnswers.unshift(answer0El.innerText);
-    codeAnswers.splice(1, 2, answer1El.innerText);
-    codeAnswers.splice(2, 1, answer2El.innerText);
-    codeAnswers.splice(3, 1, answer3El.innerText)
-    console.log(codeAnswers)
+
+function updateDivs() {
+    playerGuess.forEach(function(num, idx) {
+    let currentDiv =  document.getElementById(`answer-${idx}`)
+    if (num === '') {
+        currentDiv.innerText = '';
+    } else {
+        currentDiv.innerText = codeOptions[num];
+    }
+    });
 }
+
 function getSecretCode() {
     secretCode = [];
     for (let i=0; i<4; i++){
-        secretCode.push(codeOptions[Math.floor(Math.random() * 6)]);
+        secretCode.push(Math.floor(Math.random() * 6));
     }
     console.log(secretCode); 
 }
 
 function getWinner() {
-    // for (var i = 0; i < secretCode.length; i++)
-    //     if (codeAnswers[i] === secretCode[i])
-    //         return winner = true;
-// secretCode.length === codeAnswers.length &&
-// secretCode.every(function (element, index) {
-//   // compare if the element matches in the same index
-//   if (element === codeAnswers[index]);
-//   return winner = true
-// });
-if (secretCode[0] === codeAnswers[0]
-    && secretCode[1] === codeAnswers[1]
-    && secretCode[2] === codeAnswers[2]
-    && secretCode[3] === codeAnswers[3])
-    return winner = true;
+    let playerCopy = playerGuess.map(val => val)
+    let codeCopy = secretCode.map(val => val)
+   feedbackArray = ['', '', '', ''];
+   //Check for correct code in correct spot
+   playerCopy.forEach(function(val, idx) {
+    if (codeCopy[idx] === parseInt(val)) {
+        feedbackArray[idx] = 'green';
+        playerCopy[idx] = 7;
+        codeCopy[idx] = 8;
+    }
+   });
+   //Check for correct code in wrong spot
+   playerCopy.forEach(function(val, idx) {
+       if (codeCopy.includes(parseInt(val))) {
+           feedbackArray[idx] = 'yellow';
+           codeCopy[codeCopy.indexOf(parseInt(val))] = 8;
+        }
+    }); 
+
+   feedbackArray.forEach(function(val, idx) {
+    console.log(val)
+       if (val === '') {
+           feedbackArray[idx] = 'purple';
+           codeCopy[codeCopy.indexOf(parseInt(val))] = 8;
+        }
+    }); 
+    console.log(feedbackArray)
+    return feedbackArray.every((val) => val === 'green')
 }
 
-function colorCheck1() {
-    if (codeAnswers[0] === secretCode[0]) {
-    answer0El.style.color = 'green'
-    }
-    else if ( codeAnswers[0] === secretCode[1] || secretCode[2] || secretCode[3]) {
-        answer0El.style.color = 'yellow'
-    }
-    else {(codeAnswers[0] !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) 
-        answer0El.style.color = 'purple'
-    }
-}
-function colorCheck2() {
-    if (codeAnswers[1] === secretCode[1]) {
-    answer1El.style.color = 'green'
-    }
-    else if (codeAnswers[1] === secretCode[0] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'yellow'
-    }
-    else if (codeAnswers[1] !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
-        answer1El.style.color = 'purple'
-    }
-} 
-function colorCheck3() {
-    if (codeAnswers[2] === secretCode[2]) {
-    answer2El.style.color = 'green'
-    }
-    else if (codeAnswers[2] === secretCode[0] || secretCode[1] || secretCode[3]) {
-    answer2El.style.color = 'yellow'
-    }
-    else if (codeAnswers[2] !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) {
-    answer2El.style.color = 'purple';
-    }
-} 
-function colorCheck4() {
-    if (codeAnswers[3] === secretCode[3]) {
-        answer3El.style.color = 'green'
-    }
-    else if (codeAnswers[3] === secretCode[0] || secretCode[1] || secretCode[2]) {
-        answer3El.style.color = 'yellow'
-    }
-    else {(codeAnswers[3] !== secretCode[0] || secretCode[1] || secretCode[2] || secretCode[3]) 
-        answer3El.style.color = 'purple';
-    }
-}
 
 
 function renderMessage() { 
@@ -161,12 +147,11 @@ function renderMessage() {
         turnPow.innerText = "Shutting Down";
         turnPow.style.color = 'red';
     }
-    
 }
+
 function renderReplay() {
-    replayBtn.style.visibility = turn === 5 ? 'visible' : 'hidden';
+    replayBtn.style.visibility = winner || turn === 5 ? 'visible' : 'hidden';
     allAnswers.style.visibility = turn == 5 || winner ? 'hidden' : 'visible';
     enterBtn.style.visibility = turn == 5 || winner ? 'hidden' : 'visible';
     answerGrid.style.visibility = turn == 5 || winner ? 'hidden' : 'visible';
-
 }
